e323be79b7799dbe2aeaab82aaf7bf2697be6013
Josh Hancock <livingicon@gmail.com>
2022-03-07 18:47:11 -0500

    Add remove button functions, positive/negative button function, and begin working on keyboard functionality and storeOperate function to work without requiring the equals key

--- script.js
+++ script.js
@@ -1,45 +1,103 @@
-//1. buttons add numerals to numbers pushing them to an array that is joined and stores the number when operation is pushed
-//2. operation is stored with number in an array and display is cleared
-//3. buttons add numerals to numbers pushing them to an array that is joined and runs the math when equal is pushed displaying value
-//4. That value is stored as the first number in the array again if another operation is pushed
+//decimal point ().tofixed(20);)?
+//keyboard friendly
 
+//NUMBER BUTTONS
+zero.addEventListener('click', display);
 one.addEventListener('click', display);
+two.addEventListener('click', display);
+three.addEventListener('click', display);
+four.addEventListener('click', display);
+five.addEventListener('click', display);
+six.addEventListener('click', display);
+seven.addEventListener('click', display);
+eight.addEventListener('click', display);
+nine.addEventListener('click', display);
+decimal.addEventListener('click', display);
+numberSigns.addEventListener('click', negative);
+window.addEventListener('keydown', useKeys);
 
-const one = document.querySelector('#1');
-const displayContent = document.querySelector('#displayContent');
+function useKeys(e) {
+  if (e.key >= 0 && e.key <= 9) {
+    display(e);
+  }
+}
 
-displayContent.textContent = `${displayValueA}`;
+//OPERATION BUTTONS
+addition.addEventListener('click', storeOperate);
+subtraction.addEventListener('click', storeOperate);
+multiplication.addEventListener('click', storeOperate);
+division.addEventListener('click', storeOperate);
+equal.addEventListener('click', equals)
 
-// function display(e) {
-//     const displayValueA = [];
-//     const displayValueB = [];
-//     if() {
-//         displayValueA.push();
-//     } else {
-//         displayValueB.push();
-//     }
-// }
+//REMOVE BUTTONS
+ce.addEventListener('click', clearEntry);
+c.addEventListener('click', refresh);
+del.addEventListener('click', backspace);
 
+const displayNum = [];
+let evaluation = [];
+let a = '';
+let b = '';
+let func = '';
 
-//const storage = [];//not in right place (conceptual)
+function display(e) {
+  if (displayNum.includes('.') === true && e.target.innerHTML === '.') {
+    return;
+  } else {
+    const displayContent = document.querySelector('#displayContent');
+    //displayNum.push(e.key);
+    displayNum.push(e.target.innerHTML);
+    displayContent.textContent = displayNum.join('');
+  }
+}
+
+function storeOperate(e) {
+  if (displayNum.length === 0 && e.target.id === "subtraction") {
+    const displayContent = document.querySelector('#displayContent');
+    displayNum.push("-");
+    displayContent.textContent = displayNum.join('');
+  } else if (evaluation.length === 0) {
+    a = Number(displayContent.textContent);
+    evaluation.push(a);
+    func = e.target.id;
+    evaluation.push(func);
+    displayContent.textContent = e.target.innerHTML;
+    displayNum.length = 0;
+  } else if (evaluation.length === 2) {
+    b = Number(displayNum.join(''));
+    operate(func, a, b);
+    evaluation.length = 0;
+    a = Number(displayContent.textContent);
+    evaluation.push(a);
+    func = e.target.id;
+    evaluation.push(func);
+    displayNum.length = 0;
+  }
+}
+
+function equals() {
+  b = Number(displayNum.join(''));
+  evaluation.push(b);
+  operate(func, a, b);
+  evaluation.length = 0;
+  displayNum.length = 0;
+  displayNum.push(displayContent.textContent);
+}
 
 //OPERATION FUNCTION
 const operate = function(func, a, b) {
-    // const a = storage[0];
-    // const func = storage[1];
-    // const b = storage[2];
     switch(func) {
       case 'addition':
-        console.log(add(a, b));
+        displayContent.textContent = add(a, b);
         break;
       case 'subtraction':
-        console.log(subtract(a, b));
+        displayContent.textContent = subtract(a, b);
         break;
       case 'multiplication':
-        console.log(multiply(a, b));
+        displayContent.textContent = multiply(a, b);
         break;
       case 'division':
-        console.log(divide(a, b));
+        displayContent.textContent = divide(a, b);
         break;
     }
   };
@@ -48,17 +106,46 @@ const operate = function(func, a, b) {
   const add = function(a, b) {
     return a + b;
   };
-  
   const subtract = function(a, b) {
     return a - b;
   };
-  
   const multiply = function(a, b) {
     return a * b;
   };
-  
   const divide = function(a, b) {
-    return a / b;
+    if (b === 0) {
+      return 'To INFINITY and BEYOND!';
+    } else {
+      return a / b;
+    }
   };
 
-  //store a, func, b in an array (always indexed the same to be called)
+  //POSITIVE and NEGATIVE BUTTON FUNCTION
+  function negative() {
+    if (Number(displayNum.join('')) > 0) {
+      const displayContent = document.querySelector('#displayContent');
+      displayNum.unshift("-");
+      displayContent.textContent = displayNum.join(''); 
+    } else {
+      const displayContent = document.querySelector('#displayContent');
+      displayNum.shift();
+      displayContent.textContent = displayNum.join('');
+    }
+  }
+
+//REMOVE BUTTON FUNCTIONS
+  function backspace() {
+    const displayContent = document.querySelector('#displayContent');
+    displayNum.pop();
+    displayContent.textContent = displayNum.join('');
+  }
+
+  function clearEntry() {
+    const displayContent = document.querySelector('#displayContent');
+    displayNum.length = 0;
+    displayContent.textContent = displayNum.join('');
+  }
+
+  function refresh() {
+    reload = location.reload();
+  }
