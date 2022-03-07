b7155bf1f6607986cc8a3cda6a68ed736bf5ab9b
Josh Hancock <livingicon@gmail.com>
2022-03-07 17:23:23 -0500

    Add initial attempt at JS display function

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
+//1. buttons add numerals to numbers pushing them to an array that is joined and stores the number when operation is pushed
+//2. operation is stored with number in an array and display is cleared
+//3. buttons add numerals to numbers pushing them to an array that is joined and runs the math when equal is pushed displaying value
+//4. That value is stored as the first number in the array again if another operation is pushed
+
+one.addEventListener('click', display);
+
+const one = document.querySelector('#1');
+const displayContent = document.querySelector('#displayContent');
+
+displayContent.textContent = `${displayValueA}`;
+
+// function display(e) {
+//     const displayValueA = [];
+//     const displayValueB = [];
+//     if() {
+//         displayValueA.push();
+//     } else {
+//         displayValueB.push();
+//     }
+// }
+
+
+//const storage = [];//not in right place (conceptual)
+
 //OPERATION FUNCTION
-const operate = function(name, a, b) {
-  switch(name) {
-    case 'addition':
-      console.log(add(a, b));
-      break;
-    case 'subtraction':
-      console.log(subtract(a, b));
-      break;
-    case 'multiplication':
-      console.log(multiply(a, b));
-      break;
-    case 'division':
-      console.log(divide(a, b));
-      break;
-  }
-};
+const operate = function(func, a, b) {
+    // const a = storage[0];
+    // const func = storage[1];
+    // const b = storage[2];
+    switch(func) {
+      case 'addition':
+        console.log(add(a, b));
+        break;
+      case 'subtraction':
+        console.log(subtract(a, b));
+        break;
+      case 'multiplication':
+        console.log(multiply(a, b));
+        break;
+      case 'division':
+        console.log(divide(a, b));
+        break;
+    }
+  };
+  
+  //BASIC MATH OPERATION FUNCTIONS
+  const add = function(a, b) {
+    return a + b;
+  };
+  
+  const subtract = function(a, b) {
+    return a - b;
+  };
+  
+  const multiply = function(a, b) {
+    return a * b;
+  };
+  
+  const divide = function(a, b) {
+    return a / b;
+  };
 
-//BASIC MATH OPERATION FUNCTIONS
-const add = function(a, b) {
-  return a + b;
-};
-
-const subtract = function(a, b) {
-  return a - b;
-};
-
-const multiply = function(a, b) {
-  return a * b;
-};
-
-const divide = function(a, b) {
-  return a / b;
-};
+  //store a, func, b in an array (always indexed the same to be called)
