0e5628854db7c04e449791e2ea3c0f5a470953fb
Josh Hancock <livingicon@gmail.com>
2022-03-07 18:44:13 -0500

    Revise display function

--- script.js
+++ script.js
@@ -1,64 +1,45 @@
-//Need to figure out how to string together problems before equals
-//Need to clear evaluation and make first number solution number?
+//1. buttons add numerals to numbers pushing them to an array that is joined and stores the number when operation is pushed
+//2. operation is stored with number in an array and display is cleared
+//3. buttons add numerals to numbers pushing them to an array that is joined and runs the math when equal is pushed displaying value
+//4. That value is stored as the first number in the array again if another operation is pushed
 
-//NUMBER BUTTONS
-zero.addEventListener('click', display);
 one.addEventListener('click', display);
-two.addEventListener('click', display);
-three.addEventListener('click', display);
-four.addEventListener('click', display);
-five.addEventListener('click', display);
-six.addEventListener('click', display);
-seven.addEventListener('click', display);
-eight.addEventListener('click', display);
-nine.addEventListener('click', display);
 
-//OPERATION BUTTONS
-addition.addEventListener('click', storeOperate);
-subtraction.addEventListener('click', storeOperate);
-multiplication.addEventListener('click', storeOperate);
-division.addEventListener('click', storeOperate);
-equal.addEventListener('click', equals)
+const one = document.querySelector('#1');
+const displayContent = document.querySelector('#displayContent');
 
-const displayNum = [];
-const evaluation = [];
+displayContent.textContent = `${displayValueA}`;
 
-function display(e) {
-  const displayContent = document.querySelector('#displayContent');
-  displayNum.push(e.target.innerHTML);
-  displayContent.textContent = `${displayNum.join('')}`;
-}
+// function display(e) {
+//     const displayValueA = [];
+//     const displayValueB = [];
+//     if() {
+//         displayValueA.push();
+//     } else {
+//         displayValueB.push();
+//     }
+// }
 
-function storeOperate(e) {
-  const numOne = displayNum.join('');
-  evaluation.push(e.target.id);
-  evaluation.push(numOne);
-  displayNum.length = 0;
-  displayContent.textContent = e.target.innerHTML;
-  console.log(evaluation);
-}
 
-function equals() {
-  const numTwo = displayNum.join('');
-  evaluation.push(numTwo);
-  console.log(numTwo);
-  console.log(evaluation);
-  operate(evaluation[0], Number(evaluation[1]), Number(evaluation[2]));
-}
+//const storage = [];//not in right place (conceptual)
+
 //OPERATION FUNCTION
 const operate = function(func, a, b) {
+    // const a = storage[0];
+    // const func = storage[1];
+    // const b = storage[2];
     switch(func) {
       case 'addition':
-        displayContent.textContent = add(a, b);
+        console.log(add(a, b));
         break;
       case 'subtraction':
-        displayContent.textContent = subtract(a, b);
+        console.log(subtract(a, b));
         break;
       case 'multiplication':
-        displayContent.textContent = multiply(a, b);
+        console.log(multiply(a, b));
         break;
       case 'division':
-        displayContent.textContent = divide(a, b);
+        console.log(divide(a, b));
         break;
     }
   };
@@ -67,12 +48,17 @@ const operate = function(func, a, b) {
   const add = function(a, b) {
     return a + b;
   };
+  
   const subtract = function(a, b) {
     return a - b;
   };
+  
   const multiply = function(a, b) {
     return a * b;
   };
+  
   const divide = function(a, b) {
     return a / b;
   };
+
+  //store a, func, b in an array (always indexed the same to be called)
